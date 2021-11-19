8cf891c9808b211bb2b522f2caf4b653d6d87c03
winniederidder <Winnie.De.Ridder@hotmail.com>
2021-11-19 01:07:03 +0100

    Override stdout instead of print to let doctests properly output

--- src/workers/python/PythonWorker.worker.ts
+++ src/workers/python/PythonWorker.worker.ts
@@ -8,9 +8,11 @@ interface LoadPyodideArgs {
     fullStdLib: boolean;
 }
 interface Pyodide {
+    runPython: (code: string, globals?: any) => any;
     runPythonAsync: (code: string) => Promise<void>;
     loadPackagesFromImports: (code: string) => Promise<void>;
     globals: Map<string, any>;
+    toPy: (obj: any) => any;
 }
 declare function importScripts(...urls: string[]): void;
 declare function loadPyodide(args: LoadPyodideArgs): Promise<Pyodide>;
@@ -21,11 +23,13 @@ importScripts("https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js");
 class PythonWorker extends Backend {
     pyodide: Pyodide;
     initialized: boolean;
+    globals: Map<string, any>;
 
     constructor() {
         super();
         this.pyodide = {} as Pyodide;
         this.initialized = false;
+        this.globals = new Map();
     }
 
     override async launch(onEvent: (e: PapyrosEvent) => void,
@@ -33,7 +37,7 @@ class PythonWorker extends Backend {
         await super.launch(onEvent, inputTextArray, inputMetaData);
         const pyodide = await loadPyodide({
             indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/",
-            fullStdLib: true
+            fullStdLib: false
         });
         this.pyodide = pyodide;
         await this.runCode(INITIALIZATION_CODE, 0);
@@ -41,13 +45,20 @@ class PythonWorker extends Backend {
         const eventCallback = (data: Map<string, any>): void =>
             this.onEvent(Object.fromEntries(data) as PapyrosEvent);
         this.pyodide.globals.get("__override_builtins")(eventCallback);
+        this.globals = new Map(this.pyodide.globals.get("__dodona_globals").toJs());
+        const originalOnEvent = this.onEvent;
+        this.onEvent = (e: PapyrosEvent) => {
+            const jsEvent: PapyrosEvent = "toJs" in e ? (e as any).toJs() as PapyrosEvent : e;
+            return originalOnEvent(jsEvent);
+        };
         this.initialized = true;
     }
 
     override async _runCodeInternal(code: string): Promise<any> {
         await this.pyodide.loadPackagesFromImports(code);
         if (this.initialized) {
-            return this.pyodide.globals.get("__run_code")(code);
+            // return this.pyodide.runPython(code, this.pyodide.toPy(new Map(this.globals)));
+            return this.pyodide.runPython(code);
         } else {
             return this.pyodide.runPythonAsync(code);
         }
