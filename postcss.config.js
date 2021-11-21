d4558870665d2886490f87fa037e6e79e0ecb309
winniederidder <Winnie.De.Ridder@hotmail.com>
2021-11-21 16:52:57 +0100

    Remove unneeded postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -3,14 +3,6 @@ module.exports = {
       require('postcss-import'),
       require('tailwindcss/nesting'),
       require('tailwindcss'),
-      require('autoprefixer'),
-      require("postcss-flexbugs-fixes"),
-      require("postcss-preset-env")({
-        autoprefixer: {
-          flexbox: "no-2009"
-        },
-        features: { 'nesting-rules': false },
-        stage: 3
-      })
+      require('autoprefixer')
     ]
   };
