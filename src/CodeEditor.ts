3d01cfee19eeaf601ec996e131d4afdd2b7af993
winniederidder <Winnie.De.Ridder@hotmail.com>
2021-12-03 19:28:33 +0100

    Finish rebase

--- src/CodeEditor.ts
+++ src/CodeEditor.ts
@@ -1,11 +1,28 @@
-import { EditorState, basicSetup } from "@codemirror/basic-setup";
-import { EditorView, keymap } from "@codemirror/view";
 import { indentWithTab } from "@codemirror/commands";
 import { javascript } from "@codemirror/lang-javascript";
+import { indentUnit, LanguageSupport } from "@codemirror/language";
+import { Compartment, EditorState, Extension } from "@codemirror/state";
 import { ProgrammingLanguage } from "./ProgrammingLanguage";
 import { python } from "@codemirror/lang-python";
-import { indentUnit, LanguageSupport } from "@codemirror/language";
-import { Compartment, Extension } from "@codemirror/state";
+import {
+    EditorView,
+    keymap, highlightSpecialChars,
+    drawSelection, highlightActiveLine
+}
+    from "@codemirror/view";
+import { history, historyKeymap } from "@codemirror/history";
+import { foldGutter, foldKeymap } from "@codemirror/fold";
+import { indentOnInput } from "@codemirror/language";
+import { lineNumbers, highlightActiveLineGutter } from "@codemirror/gutter";
+import { defaultKeymap } from "@codemirror/commands";
+import { bracketMatching } from "@codemirror/matchbrackets";
+import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets";
+import { searchKeymap, highlightSelectionMatches } from "@codemirror/search";
+import { autocompletion, completionKeymap } from "@codemirror/autocomplete";
+import { commentKeymap } from "@codemirror/comment";
+import { rectangularSelection } from "@codemirror/rectangular-selection";
+import { defaultHighlightStyle } from "@codemirror/highlight";
+import { lintKeymap } from "@codemirror/lint";
 
 function getLanguageSupport(language: ProgrammingLanguage): LanguageSupport {
     switch (language) {
@@ -21,16 +38,67 @@ function getLanguageSupport(language: ProgrammingLanguage): LanguageSupport {
     }
 }
 
+/*
+*  - [syntax highlighting depending on the language](#getLanguageSupport)
+*  - [line numbers](#gutter.lineNumbers)
+*  - [special character highlighting](#view.highlightSpecialChars)
+*  - [the undo history](#history.history)
+*  - [a fold gutter](#fold.foldGutter)
+*  - [custom selection drawing](#view.drawSelection)
+*  - [multiple selections](#state.EditorState^allowMultipleSelections)
+*  - [reindentation on input](#language.indentOnInput)
+*  - [the default highlight style](#highlight.defaultHighlightStyle) (as fallback)
+*  - [bracket matching](#matchbrackets.bracketMatching)
+*  - [bracket closing](#closebrackets.closeBrackets)
+*  - [autocompletion](#autocomplete.autocompletion)
+*  - [rectangular selection](#rectangular-selection.rectangularSelection)
+*  - [active line highlighting](#view.highlightActiveLine)
+*  - [active line gutter highlighting](#gutter.highlightActiveLineGutter)
+*  - [selection match highlighting](#search.highlightSelectionMatches)
+* Keymaps:
+*  - [the default command bindings](#commands.defaultKeymap)
+*  - [search](#search.searchKeymap)
+*  - [commenting](#comment.commentKeymap)
+*  - [linting](#lint.lintKeymap)
+*  - [indenting with tab](#commands.indentWithTab)
+*/
+function getExtensions(): Array<Extension> {
+    return [
+        lineNumbers(),
+        highlightActiveLineGutter(),
+        highlightSpecialChars(),
+        history(),
+        foldGutter(),
+        drawSelection(),
+        EditorState.allowMultipleSelections.of(true),
+        indentOnInput(),
+        defaultHighlightStyle.fallback,
+        bracketMatching(),
+        closeBrackets(),
+        autocompletion(),
+        rectangularSelection(),
+        highlightActiveLine(),
+        highlightSelectionMatches(),
+        keymap.of([
+            ...closeBracketsKeymap,
+            ...defaultKeymap,
+            ...searchKeymap,
+            ...historyKeymap,
+            ...foldKeymap,
+            ...commentKeymap,
+            ...completionKeymap,
+            ...lintKeymap
+        ]),
+        keymap.of([indentWithTab])
+    ];
+}
+
 function getEditorView(parentElement: HTMLElement,
-    initialCode = "", extensions?: Extension[]): EditorView {
+    initialCode = "", extensions: Extension[] = []): EditorView {
     return new EditorView({
         state: EditorState.create({
             doc: initialCode,
-            extensions: [
-                extensions || [],
-                basicSetup,
-                keymap.of([indentWithTab])
-            ]
+            extensions: extensions
         }),
         parent: parentElement
     });
@@ -52,7 +120,9 @@ export class CodeEditor {
         this.editorView = getEditorView(element, initialCode,
             [
                 this.languageCompartment.of(getLanguageSupport(language)),
-                this.indentCompartment.of(indentUnit.of(getIndentUnit(indentLength)))
+                this.indentCompartment.of(indentUnit.of(getIndentUnit(indentLength))),
+                keymap.of([indentWithTab]),
+                ...getExtensions()
             ]);
         element.replaceChildren(this.editorView.dom);
     }
--- src/Papyros.ts
+++ src/Papyros.ts
@@ -5,11 +5,7 @@ import { Backend } from "./Backend";
 import { getBackend, stopBackend } from "./BackendManager";
 import { CodeEditor } from "./CodeEditor";
 import {
-<<<<<<< HEAD
     APPLICATION_STATE_TEXT_ID, EDITOR_WRAPPER_ID, DEFAULT_LOCALE, DEFAULT_PROGRAMMING_LANGUAGE, INPUT_RELATIVE_URL,
-=======
-    APPLICATION_STATE_TEXT_ID, CODE_TA_ID, DEFAULT_LOCALE, DEFAULT_PROGRAMMING_LANGUAGE, INPUT_RELATIVE_URL,
->>>>>>> Add support for rendering from a given element with I18n
     INPUT_TA_ID, LANGUAGE_SELECT_ID, OUTPUT_TA_ID,
     RUN_BTN_ID, STATE_SPINNER_ID, TERMINATE_BTN_ID
 } from "./Constants";
@@ -20,7 +16,6 @@ import { LogType, papyrosLog } from "./util/Logging";
 
 function loadTranslations(): void {
     for (const [language, translations] of Object.entries(TRANSLATIONS)) {
-<<<<<<< HEAD
         // Add keys to already existing translations if they exist
         I18n.translations[language] = Object.assign((I18n.translations[language] || {}), translations);
     }
@@ -182,170 +177,6 @@ export class Papyros {
         this.stateManager.runButton.addEventListener("click", () => this.runCode());
         this.stateManager.terminateButton.addEventListener("click", () => this.terminate());
 
-=======
-        I18n.translations[language] = translations;
-    }
-}
-
-const t = I18n.t;
-
-function renderPapyros(parent: HTMLElement, programmingLanguage: ProgrammingLanguage): void {
-    const options = PROGRAMMING_LANGUAGES.map(pl => {
-        const selected = programmingLanguage === pl ? "selected" : "";
-        return `<option ${selected}} value="${pl}">${t(pl)}</option>`;
-    }).join("\n");
-    parent.innerHTML =
-        `
-    <div id="papyros" class="max-h-screen h-full overflow-y-hidden">
-    <div class="bg-blue-500 text-white text-lg p-4">
-      ${t("Papyros")}
-    </div>
-    <div class="m-10">
-      <!-- Header -->
-      <div class="flex flex-row items-center">
-        <label for="language-select">Programming language</label>
-        <select id="language-select" class="m-2 border-2">
-          ${options}
-        </select>
-        <button id="run-code-btn" type="button"
-          class="text-white bg-blue-500 border-2 m-3 px-4 inset-y-2 rounded-lg justify-items-center">
-            ${t("run")}
-        </button>
-        <button id="terminate-btn" type="button" class="mr-1 btn btn-danger" hidden>
-            ${t("terminate")}
-        </button>
-        <div class="flex flex-row items-center">
-          <svg id="state-spinner" class="animate-spin mr-3 h-5 w-5 text-white"
-           xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" display="none">
-          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="red" stroke-width="4"></circle>
-          <path class="opacity-75" fill="red"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-            </path>
-          </svg>
-          <div id="application-state-text">${t("loading")}</div>
-        </div>
-      </div>
-
-      <!--Body of the application-->
-      <div class="grid grid-cols-2 gap-4 box-border max-h-full">
-        <!--Left code section-->
-        <div class="col-span-1">
-          <h1>${t("enter code")}:</h1>
-          <div id="code-area" class="overflow-auto max-h-full"></div>
-        </div>
-        <!--Right user input and output section-->
-        <div class="col-span-1">
-          <h1>${t("enter input")}:</h1>
-          <textarea id="code-input-area" class="border-2 h-auto w-full max-h-1/4 overflow-auto" rows="5"></textarea>
-          <h1>${t("code output")}:</h1>
-          <textarea id="code-output-area" readonly class="border-2 w-full min-h-1/5 max-h-3/5 overflow-auto"></textarea>
-        </div>
-      </div>
-    </div>
-  </div>
-    `;
-}
-
-
-enum PapyrosState {
-    Loading = "loading",
-    Running = "running",
-    AwaitingInput = "awaiting input",
-    Terminating = "terminating",
-    Ready = "ready"
-}
-
-class PapyrosStateManager {
-    state: PapyrosState;
-    stateSpinner: HTMLElement;
-    stateText: HTMLElement;
-
-    constructor(state: PapyrosState) {
-        this.stateSpinner = document.getElementById(STATE_SPINNER_ID) as HTMLElement;
-        this.stateText = document.getElementById(APPLICATION_STATE_TEXT_ID) as HTMLElement;
-        this.state = state;
-    }
-
-    setState(state: PapyrosState): void {
-        if (state !== this.state) {
-            this.state = state;
-            if (state === PapyrosState.Ready) {
-                this.stateSpinner.style.display = "none";
-                this.stateText.innerText = "";
-            } else {
-                this.stateSpinner.style.display = "";
-                this.stateText.innerText = t(state);
-            }
-        }
-    }
-}
-
-interface PapyrosCodeState {
-    programmingLanguage: ProgrammingLanguage;
-    editor: CodeEditor;
-    backend: Remote<Backend>;
-    languageSelect: HTMLSelectElement;
-    codeElement: HTMLInputElement;
-    runId: number;
-    runButton: HTMLButtonElement;
-    terminateButton: HTMLButtonElement;
-    outputArea: HTMLInputElement;
-}
-
-interface PapyrosInputState {
-    lineNr: number;
-    textEncoder: TextEncoder;
-    inputArea: HTMLInputElement;
-    inputTextArray?: Uint8Array;
-    inputMetaData?: Int32Array;
-}
-
-export class Papyros {
-    stateManager: PapyrosStateManager;
-    codeState: PapyrosCodeState;
-    inputState: PapyrosInputState;
-
-    constructor(parent: HTMLElement, locale: string, programmingLanguage: ProgrammingLanguage,
-        inputTextArray?: Uint8Array, inputMetaData?: Int32Array) {
-        this.stateManager = new PapyrosStateManager(PapyrosState.Loading);
-        this.codeState = {
-            programmingLanguage: programmingLanguage,
-            editor: new CodeEditor(
-                document.getElementById(CODE_TA_ID) as HTMLInputElement, programmingLanguage),
-            backend: {} as Remote<Backend>,
-            languageSelect: document.getElementById(LANGUAGE_SELECT_ID) as HTMLSelectElement,
-            codeElement: document.getElementById(CODE_TA_ID) as HTMLInputElement,
-            outputArea: document.getElementById(OUTPUT_TA_ID) as HTMLInputElement,
-            runId: 0,
-            runButton: document.getElementById(RUN_BTN_ID) as HTMLButtonElement,
-            terminateButton: document.getElementById(TERMINATE_BTN_ID) as HTMLButtonElement,
-        };
-
-        this.inputState = {
-            lineNr: 0,
-            textEncoder: new TextEncoder(),
-            inputArea: document.getElementById(INPUT_TA_ID) as HTMLInputElement,
-            inputTextArray: inputTextArray,
-            inputMetaData: inputMetaData
-        };
-    }
-
-    async launch(): Promise<Papyros> {
-        this.codeState.languageSelect.addEventListener("change",
-            async () => {
-                const {
-                    languageSelect, editor, backend
-                } = this.codeState;
-                const language = plFromString(languageSelect.value);
-                stopBackend(backend);
-                editor.setLanguage(language, editor.getCode());
-                await this.startBackend();
-            }
-        );
-        this.codeState.runButton.addEventListener("click", () => this.runCode());
-        this.codeState.terminateButton.addEventListener("click", () => this.terminate());
-
->>>>>>> Add support for rendering from a given element with I18n
         this.inputState.inputArea.onkeydown = e => {
             papyrosLog(LogType.Debug, "Key down in inputArea", e);
             if (this.stateManager.state === PapyrosState.AwaitingInput &&
@@ -354,7 +185,6 @@ export class Papyros {
                 this.sendInput();
             }
         };
-<<<<<<< HEAD
         await this.startBackend();
         return this;
     }
@@ -371,54 +201,23 @@ export class Papyros {
     async startBackend(): Promise<void> {
         const {
             programmingLanguage
-=======
-
-        return this;
-    }
-
-    setProgrammingLanguage(programmingLanguage: ProgrammingLanguage): void {
-        if (this.codeState.programmingLanguage !== programmingLanguage) {
-            stopBackend(this.codeState.backend);
-            this.codeState.programmingLanguage = programmingLanguage;
-            this.codeState.editor.setLanguage(programmingLanguage);
-        }
-    }
-
-    async startBackend(): Promise<void> {
-        const {
-            runButton, programmingLanguage
->>>>>>> Add support for rendering from a given element with I18n
         } = this.codeState;
         const {
             inputTextArray, inputMetaData
         } = this.inputState;
         this.stateManager.setState(PapyrosState.Loading);
-<<<<<<< HEAD
         const backend = getBackend(programmingLanguage);
         await backend.launch(proxy(e => this.onMessage(e)), inputTextArray, inputMetaData);
         this.codeState.backend = backend;
         this.stateManager.setState(PapyrosState.Ready);
-=======
-        runButton.disabled = true;
-        const backend = getBackend(programmingLanguage);
-        await backend.launch(proxy(e => this.onMessage(e)), inputTextArray, inputMetaData);
-        runButton.disabled = false;
-        this.stateManager.setState(PapyrosState.Ready);
-        return Promise.resolve();
->>>>>>> Add support for rendering from a given element with I18n
     }
 
     static fromElement(parent: HTMLElement, locale = DEFAULT_LOCALE, programmingLanguage = DEFAULT_PROGRAMMING_LANGUAGE,
         inputTextArray?: Uint8Array, inputMetaData?: Int32Array): Promise<Papyros> {
         loadTranslations();
-<<<<<<< HEAD
+        renderPapyros(parent, programmingLanguage);
         I18n.locale = locale;
-        renderPapyros(parent, programmingLanguage);
         return new Papyros(programmingLanguage, inputTextArray, inputMetaData).launch();
-=======
-        renderPapyros(parent, programmingLanguage);
-        return new Papyros(parent, locale, programmingLanguage, inputTextArray, inputMetaData).launch();
->>>>>>> Add support for rendering from a given element with I18n
     }
 
 
@@ -487,19 +286,11 @@ export class Papyros {
     async runCode(): Promise<void> {
         this.codeState.runId += 1;
         this.stateManager.setState(PapyrosState.Running);
-<<<<<<< HEAD
         this.codeState.outputArea.value = "";
-=======
-        this.codeState.runButton.disabled = true;
-        this.inputState.lineNr = 0;
-        this.codeState.outputArea.value = "";
-        this.codeState.terminateButton.hidden = false;
->>>>>>> Add support for rendering from a given element with I18n
         papyrosLog(LogType.Debug, "Running code in Papyros, sending to backend");
-        // const start = new Date().getTime();
+        const start = new Date().getTime();
         try {
             await this.codeState.backend.runCode(
-<<<<<<< HEAD
                 this.codeState.editor.getCode(), this.codeState.runId);
         } catch (error: any) {
             this.onError(error);
@@ -508,17 +299,6 @@ export class Papyros {
             this.stateManager.setState(PapyrosState.Ready, t("Papyros.finished", { time: end - start }));
             this.inputState.inputArea.value = "";
             this.inputState.lineNr = 0;
-=======
-                this.codeState.codeElement.value, this.codeState.runId);
-        } catch (error: any) {
-            this.onError(error);
-        } finally {
-            // const end = new Date().getTime();
-            this.stateManager.setState(PapyrosState.Ready);
-            // stateText.innerText = `Code executed in ${end - start} ms`;
-            // terminateButton.hidden = true;
-            // runButton.disabled = false;
->>>>>>> Add support for rendering from a given element with I18n
         }
     }
 
@@ -526,10 +306,6 @@ export class Papyros {
         papyrosLog(LogType.Debug, "Called terminate, stopping backend!");
         this.codeState.runId += 1; // ignore messages coming from last run
         this.stateManager.setState(PapyrosState.Terminating);
-<<<<<<< HEAD
-=======
-        // terminateButton.hidden = true;
->>>>>>> Add support for rendering from a given element with I18n
         stopBackend(this.codeState.backend);
         return this.startBackend();
     }
