06e5c195e975de6e73db4f7660b0f180cc16845f
winniederidder <Winnie.De.Ridder@hotmail.com>
2021-12-06 12:57:59 +0100

    Make script output formatted text for annotations

--- scripts/ValidateTranslations.js
+++ scripts/ValidateTranslations.js
@@ -1,11 +1,21 @@
 const extract = require("i18n-extract");
 const translations = require("../src/Translations.js");
+const fs = require("fs");
 
+const outputFile = fs.openSync("translationIssues.txt", "w");
 function checkReport(report, type, locale) {
     const valid = report.length === 0;
     if (!valid) {
         console.log(`Found ${report.length} ${type} keys for locale: ${locale}.`);
         console.log(report);
+        console.log(JSON.stringify(report));
+        for (const issue of report) {
+            const {
+                line, column
+            } = issue.loc.start;
+            const errorMsg = `${type} key ${issue.key} for locale ${locale}`;
+            fs.writeFileSync(outputFile, `${issue.file}:${line}:${column}: ${errorMsg}\n`);
+        }
     }
     return valid;
 }
@@ -36,7 +46,8 @@ const checks = [
     {
         "type": "dynamic",
         "check": extract.forbidDynamic,
-        "allowed": ["Papyros.programming_languages.*", "Papyros.locales.*", "Papyros.states.*"]
+        // "allowed": ["Papyros.programming_languages.*", "Papyros.locales.*", "Papyros.states.*"]
+        "allowed": []
     }
 ];
 
