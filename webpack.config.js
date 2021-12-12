4c2c724a169ba6b2abbbdd381ff0c6b00745232d
winniederidder <Winnie.De.Ridder@hotmail.com>
2021-12-12 15:22:21 +0100

    Update webpack and tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,43 @@
 const path = require("path");
 
-module.exports = function(webpackEnv){
+module.exports = function (webpackEnv) {
 	const mode = webpackEnv["WEBPACK_SERVE"] ? "development" : "production";
+	const outDir = mode === "development" ? "public" : "lib";
+	const entry = mode === "development" ? "./src/index.ts" : "./src/library.ts";
 	return {
-	  entry: "./src/index.ts",
-	  module: {
-		rules: [
-		  {
-			test: /\.tsx?$/,
-			use: "ts-loader",
-			exclude: /node_modules/,
-		  },
-		  {
-			test: /\.css$/i,
-			use: [
-			  "style-loader",
-			  "css-loader",
-			  "postcss-loader"
+		entry: entry,
+		module: {
+			rules: [
+				{
+					test: /\.ts?$/,
+					use: "ts-loader",
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.css$/i,
+					use: [
+						"style-loader",
+						"css-loader",
+						"postcss-loader"
+					],
+				}
 			],
-	  	  }
-		],
-	  },
-	  resolve: {
-		extensions: [".tsx", ".ts", ".js"],
-	  },
-	  output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "build"),
-	  },
-	  devtool: "inline-source-map",
-	  mode: mode,
-	  devServer: {
-		contentBase: path.join(__dirname, "build"),
-	  },
-    }
+		},
+		resolve: {
+			extensions: [".ts", ".js"],
+		},
+		output: {
+			filename: "index.js",
+			path: path.resolve(__dirname, outDir),
+			library: {
+				name: "Papyros",
+				type: "umd"
+			}
+		},
+		mode: mode,
+		target: "web",
+		devServer: {
+			contentBase: path.join(__dirname, "public"),
+		},
+	}
 };
