9171e3488d049e00cc35ba04f2777b1c1fe7368c
winniederidder <Winnie.De.Ridder@hotmail.com>
2021-12-17 10:31:54 +0100

    Make class to show output and errors

---
+++ src/OutputManager.ts
@@ -0,0 +1,77 @@
+import { inCircle } from "./util/HTMLShapes";
+
+export interface FriendlyError {
+    name: string;
+    traceback?: string;
+    info?: string;
+    why?: string;
+    what?: string;
+    where?: string;
+    suggestions?: string;
+}
+
+function newLine(): string {
+    return "<br/>";
+}
+
+export class OutputManager {
+    outputArea: HTMLElement;
+    constructor(outputWrapperId: string) {
+        this.outputArea = document.getElementById(outputWrapperId) as HTMLElement;
+    }
+
+    renderNextElement(html: string): void {
+        this.outputArea.insertAdjacentHTML("beforeend", html);
+    }
+
+    newLine(): void {
+        this.renderNextElement("<br/>");
+    }
+
+    spanify(text: string, ignoreEmpty = false): string {
+        if (text === "\n") {
+            return newLine();
+        } else if (text.includes("\n")) {
+            return text.split("\n")
+                .filter(text => !ignoreEmpty || text.trim().length > 0)
+                .map(text => `<span>${text}</span><br/>`)
+                .join("\n");
+        } else {
+            return `<span>${text}</span>`;
+        }
+    }
+
+    showOutput(output: string): void {
+        this.renderNextElement(this.spanify(output));
+    }
+
+    showError(error: FriendlyError): void {
+        console.log("Got friendly error: ", error);
+        let errorHTML = "";
+        const shortTraceback = (error.where || error.traceback || "").trim();
+        errorHTML +=
+            `<div class="text-red-500 text-bold">
+                ${inCircle("?", error.info)}${error.name} traceback: <br/>
+                ${this.spanify(shortTraceback, true)}
+            </div>`;
+        errorHTML += newLine();
+        if (error.what) {
+            errorHTML += this.spanify(error.what.trim(), true) + newLine();
+        }
+        if (error.why) {
+            errorHTML += this.spanify(error.why.trim(), true) + newLine();
+        }
+        if (error.suggestions) {
+            errorHTML += this.spanify(error.suggestions, true) + newLine();
+        }
+        this.renderNextElement(errorHTML);
+    }
+
+    onRunStart(): void {
+        this.outputArea.textContent = "";
+    }
+
+    onRunEnd(): void {
+        // currently empty
+    }
+}
