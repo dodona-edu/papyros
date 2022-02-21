8197ec363429b632b420468f03f4f91cf0406869
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-02-21 11:59:49 +0100

    Add missing items

--- src/library.ts
+++ src/library.ts
@@ -1,18 +1,18 @@
 import { CodeEditor } from "./CodeEditor";
-import { InputManager } from "./InputManager";
+import { InputManager, InputMode } from "./InputManager";
 import { OutputManager } from "./OutputManager";
 import { Papyros } from "./Papyros";
 import { PapyrosEvent } from "./PapyrosEvent";
-import { ProgrammingLanguage } from "./ProgrammingLanguage";
 import { StatusPanel } from "./StatusPanel";
 
+export * from "./ProgrammingLanguage";
 export type { PapyrosEvent };
 export {
     Papyros,
     CodeEditor,
     StatusPanel,
     InputManager,
+    InputMode,
     OutputManager,
-    ProgrammingLanguage
 };
 
