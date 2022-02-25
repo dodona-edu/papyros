be7c854105f5e1c1276fe8c5d9957f4f8c6ab65c
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-02-25 21:56:22 +0100

    Add script for default worker

---
+++ scripts/InstallWorker.sh
@@ -0,0 +1,7 @@
+publicDir="public"
+if [ $# -eq 1 ]; then
+    publicDir=$1
+fi
+serviceWorkerPath=$publicDir/inputServiceWorker.js
+cp ../node_modules/@dodona/payros/inputServiceWorker.js $serviceWorkerPath
+echo Installed default input service worker at $serviceWorkerPath
