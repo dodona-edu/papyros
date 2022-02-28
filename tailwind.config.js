053b3ff84625565824c878c183ef8024e41bf442
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-02-28 21:35:07 +0100

    Docs in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
+// Scale sizes according to 80vh to prevent overflows
+const HEIGHTS = {
+  "0": "0",
+  "1/5": "16vh",
+  "1/4": "20vh",
+  "2/5": "32vh",
+  "1/2": "40vh",
+  "3/5": "48vh",
+  "3/4": "60vh",
+  "4/5": "64vh",
+  "9/10": "72vh",
+  "full": "80vh"
+};
 module.exports = {
   purge: [
-    "./build/**/*.html",
     "./src/**/*.ts",
   ],
   variants: {
     extend: {
+      // generate classes for these states
       backgroundColor: ["active"],
       opacity: ['disabled'],
       cursor: ['disabled', 'hover'],
@@ -13,29 +26,8 @@ module.exports = {
   },
   theme: {
     extend: {
-      maxHeight: {
-        "0": "0",
-        "1/5": "16vh",
-        "1/4": "20vh",
-        "2/5": "32vh",
-        "1/2": "40vh",
-        "3/5": "48vh",
-        "3/4": "60vh",
-        "4/5": "64vh",
-        "9/10": "72vh",
-        "full": "80vh"
-      },
-      minHeight: {
-        "0": "0",
-        "1/5": "16vh",
-        "1/4": "20vh",
-        "2/5": "32vh",
-        "1/2": "40vh",
-        "3/5": "48vh",
-        "3/4": "60vh",
-        "4/5": "64vh",
-        "full": "80vh"
-      }
+      maxHeight: HEIGHTS,
+      minHeight: HEIGHTS
     }
   }
 };
