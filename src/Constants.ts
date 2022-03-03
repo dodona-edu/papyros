41fa2c91d4e7374a23bd7981984e6f195eaddb95
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-03-03 23:22:45 +0100

    docs on comments

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,26 +1,22 @@
 import { ProgrammingLanguage } from "./ProgrammingLanguage";
-
+/* Default HT%M ids for various elements */
 export const MAIN_APP_ID = "papyros";
-
 export const OUTPUT_TA_ID = "code-output-area";
 export const INPUT_AREA_WRAPPER_ID = "code-input-area-wrapper";
 export const INPUT_TA_ID = "code-input-area";
 export const EDITOR_WRAPPER_ID = "code-area";
 export const PANEL_WRAPPER_ID = "code-status-panel";
-
 export const STATE_SPINNER_ID = "state-spinner";
 export const APPLICATION_STATE_TEXT_ID = "application-state-text";
-
 export const RUN_BTN_ID = "run-code-btn";
 export const STOP_BTN_ID = "stop-btn";
 export const SEND_INPUT_BTN_ID = "send-input-btn";
 export const SWITCH_INPUT_MODE_A_ID = "switch-input-mode";
-
-export const PROGRAMMING_LANGUAGE_SELECT_ID = "programming-language-select";
-export const DEFAULT_PROGRAMMING_LANGUAGE = ProgrammingLanguage.Python;
 export const EXAMPLE_SELECT_ID = "example-select";
-
 export const LOCALE_SELECT_ID = "locale-select";
-export const DEFAULT_LOCALE = "nl";
+export const PROGRAMMING_LANGUAGE_SELECT_ID = "programming-language-select";
 
+/* Default values for various properties */
+export const DEFAULT_PROGRAMMING_LANGUAGE = ProgrammingLanguage.Python;
+export const DEFAULT_LOCALE = "nl";
 export const DEFAULT_SERVICE_WORKER = "inputServiceWorker.js";
