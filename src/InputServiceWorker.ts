6e2adfa0e6a89ee6a44a030a8fc78cd813177f1d
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-03-03 23:30:57 +0100

    Docs for inputserviceworker

--- src/InputServiceWorker.ts
+++ src/InputServiceWorker.ts
@@ -1,15 +1,18 @@
 /**
  * Default service worker to process user input using HTTP requests
  */
+// Impport service worker provided by the Papyros-package
 import { InputWorker } from "./workers/input/InputWorker";
 
-// Impport service worker provided by the Papyros-package
+// Strip away the filename of the script to obtain the scope
 let domain = location.href;
 domain = domain.slice(0, domain.lastIndexOf("/")+1);
 const inputHandler = new InputWorker(domain);
 
 addEventListener("fetch", async function (event: FetchEvent) {
     if (!await inputHandler.handleInputRequest(event)) {
+        // Not a Papyros-specific request
+        // Fetch as we would handle a normal request
         event.respondWith(fetch(event.request));
     }
 });
