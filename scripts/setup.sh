548776dcfc1587bc2479dcc2339af97e23f2e5a9
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-04-03 00:29:22 +0200

    Remove some unwanted files from the tar ~3MB

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,4 +11,9 @@ mkdir $OUT_DIR
 pip install -t $OUT_DIR python-runner friendly_traceback jedi
 cp -r $SRC_DIR $OUT_DIR
 cd $OUT_DIR
+# Cleanup unneeded folders in the tar
+for file in $(find . -name '*__pycache__*' -or -name '*dist-info' -or -name '*locales')
+do
+    rm -rf $file
+done
 tar cf ../$PACKAGE_NAME.tar.load_by_url *
