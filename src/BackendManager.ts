6c8952bbaba19b004aecc902994ca08bc7b9e4d7
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-04-04 15:13:22 +0200

    Refactor and allow adding new backends at runtime

--- src/BackendManager.ts
+++ src/BackendManager.ts
@@ -13,63 +13,52 @@ import { PyodideClient } from "pyodide-worker-runner";
  */
 type BackendEventListener = (e: BackendEvent) => void;
 
-class Cacheable<T> {
-    private cached: T | null;
-    private createFn: () => T;
-    constructor(createFn: () => T) {
-        this.cached = null;
-        this.createFn = createFn;
-    }
-    public get(): T {
-        if (this.cached === null) {
-            this.cached = this.createFn();
-        }
-        return this.cached;
-    }
-}
-
 /**
  * Abstract class to implement the singleton pattern
  * Static methods group functionality
  */
 export abstract class BackendManager {
-    static createWorkerMap: Map<ProgrammingLanguage, Cacheable<SyncClient<Backend>>>
-        = BackendManager.buildSyncClientMap();
-    static channel: Channel = makeChannel()!;
+    /**
+     * Map programming languages to Backend constructors
+     */
+    private static createBackendMap: Map<ProgrammingLanguage, () => SyncClient<Backend>>;
+    /**
+     * Map to cache Backends per ProgrammingLanguage
+     */
+    private static backendMap: Map<ProgrammingLanguage, SyncClient<Backend>>;
     /**
      * Map an event type to interested subscribers
      * Uses an Array to maintain order of subscription
      */
-    static subscriberMap: Map<BackendEventType, Array<BackendEventListener>> = new Map();
-
-    private static buildSyncClientMap(): Map<ProgrammingLanguage, Cacheable<SyncClient<Backend>>> {
-        const m = new Map();
-        m.set(
-            ProgrammingLanguage.Python, new Cacheable(
-                () => new PyodideClient<Backend>(
-                    () => new PythonWorker(),
-                    BackendManager.channel
-                )
-            )
-        );
-        m.set(
-            ProgrammingLanguage.JavaScript, new Cacheable(
-                () => new SyncClient<Backend>(
-                    () => new JavaScriptWorker(),
-                    BackendManager.channel
-                )
-            )
-        );
-        return m;
-    }
+    static subscriberMap: Map<BackendEventType, Array<BackendEventListener>>;
     /**
-     * Start a backend for the given language, while storing the worker
-     * @param {ProgrammingLanguage} language The programming language supported by the backend
-     * @return {Remote<Backend>} A Comlink proxy for the Backend
+     * The channel used to communicate with the SyncClients
      */
-    static startBackend(language: ProgrammingLanguage): SyncClient<Backend> {
-        if (this.createWorkerMap.has(language)) {
-            return this.createWorkerMap.get(language)!.get();
+    static channel: Channel;
+
+    /**
+     * @param {ProgrammingLanguage} language The language to support
+     * @param {Function} backendCreator The constructor for a SyncClient
+     */
+    static registerBackend(language: ProgrammingLanguage,
+        backendCreator: () => SyncClient<Backend>): void {
+        BackendManager.createBackendMap.set(language, backendCreator);
+        BackendManager.backendMap.delete(language);
+    }
+
+    /**
+     * Start a backend for the given language and cache for reuse
+     * @param {string} language The programming language supported by the backend
+     * @return {SyncClient<Backend>} A SyncClient for the Backend
+     */
+    static startBackend(language: string): SyncClient<Backend> {
+        if (this.backendMap.has(language)) { // Cached
+            return this.backendMap.get(language)!;
+        } else if (this.createBackendMap.has(language)) {
+            // Create and then cache
+            const syncClient = this.createBackendMap.get(language)!();
+            this.backendMap.set(language, syncClient);
+            return syncClient;
         } else {
             throw new Error(`${language} is not yet supported.`);
         }
@@ -100,5 +89,27 @@ export abstract class BackendManager {
         if (this.subscriberMap.has(e.type)) {
             this.subscriberMap.get(e.type)!.forEach(cb => cb(e));
         }
+    }
+
+    /**
+     * Initialise the fields and setup the maps
+     */
+    static {
+        BackendManager.channel = makeChannel()!;
+        BackendManager.createBackendMap = new Map();
+        BackendManager.backendMap = new Map();
+        BackendManager.subscriberMap = new Map();
+        BackendManager.registerBackend(ProgrammingLanguage.Python,
+            () => new PyodideClient<Backend>(
+                () => new PythonWorker(),
+                BackendManager.channel
+            )
+        );
+        BackendManager.registerBackend(ProgrammingLanguage.JavaScript,
+            () => new SyncClient<Backend>(
+                () => new JavaScriptWorker(),
+                BackendManager.channel
+            )
+        );
     }
 }
