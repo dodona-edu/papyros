f172a4e4e57fcc34f49668a75de51c453144a93b
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-04-05 22:53:08 +0200

    Add tar to github

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Expects to be run from the main directory
 rm -rf dist
 bash ./scripts/setup.sh
-mkdir dist && mkdir dist/workers && mkdir dist/workers/python
-mv src/workers/python/python_package.tar.gz.load_by_url dist/workers/python/python_package.tar.gz.load_by_url
 webpack build --mode=$1
-rm dist/workers/python/python_package.tar.gz.load_by_url
