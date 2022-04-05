2139b24e8ec82fe7d36c8048e8e3a1f7e3ace4b8
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-04-05 21:41:58 +0200

    Use python script to generate compressed tar

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-# Expects to be run from the main directory
-BASE_PYTHON_DIR=src/workers/python
-PACKAGE_NAME=python_package
-OUT_DIR=$BASE_PYTHON_DIR/$PACKAGE_NAME
-SRC_DIR=$BASE_PYTHON_DIR/papyros
+cd src/workers/python
+python3.9 build_package.py
 
-set -eu
-rm -rf $OUT_DIR $OUT_DIR.tar.load_by_url
-mkdir $OUT_DIR
-pip install -t $OUT_DIR python-runner friendly_traceback jedi
-cp -r $SRC_DIR $OUT_DIR
-cd $OUT_DIR
-# Cleanup unneeded folders in the tar
-for file in $(find . -name '*__pycache__*' -or -name '*dist-info' -or -name '*locales')
-do
-    rm -rf $file
-done
-tar cf ../$PACKAGE_NAME.tar.load_by_url *
+
---
+++ src/workers/python/build_package.py
@@ -0,0 +1,49 @@
+
+import tarfile
+import shutil
+import os
+import subprocess
+import sys
+
+def tarfile_filter(tar_info):
+    name = tar_info.name
+    if any(
+        x in name
+        for x in [
+            "__pycache__",
+            "friendly_traceback/locales",
+            "dist-info"
+        ]
+    ) or name.endswith(".pyc"):
+        return None
+    return tar_info
+
+def create_package(package_name, dependencies, extra_deps):
+    shutil.rmtree(package_name, ignore_errors=True)
+    install_dependencies(dependencies.split(" "), package_name)
+    try:
+        shutil.copytree(extra_deps,
+        os.path.join(package_name, extra_deps), dirs_exist_ok=True)
+    except Exception as e:
+        # Always seems to result in a harmless permission denied error
+        print(e)
+        pass
+    tar_name = f"{package_name}.tar.gz.load_by_url"
+    if os.path.exists(tar_name):
+        os.remove(tar_name)
+    with tarfile.open(tar_name, "w:gz") as tar:
+        tar.add(package_name, arcname="", recursive=True, filter=tarfile_filter)
+    shutil.rmtree(package_name)
+
+def install_dependencies(packages, out_dir):
+    if not isinstance(packages, list):
+        packages = [packages]
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-t", out_dir, *packages])
+
+def check_tar(tarname):
+    with open(tarname, "rb") as t:
+        shutil.unpack_archive(tarname, ".", 'gztar')
+
+
+if __name__ == "__main__":
+    create_package("python_package", "python-runner friendly_traceback jedi", extra_deps="papyros")
