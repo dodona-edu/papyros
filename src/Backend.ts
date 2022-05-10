2ed75a8de9da3dbe0f49c2ea4612f7747fdcfe8d
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-05-10 18:58:34 +0200

    Improve linting and alleviate double display issue

--- src/Backend.ts
+++ src/Backend.ts
@@ -39,9 +39,30 @@ export interface WorkerAutocompleteContext {
 }
 
 export interface WorkerDiagnostic {
+    /**
+     * 1-based index of the starting line containing the issue
+     */
     lineNr: number;
+    /**
+     * 0-based index of the column in the starting line
+     */
     columnNr: number;
+    /**
+     * 1-based index of the ending line containing the issue
+     * Can be the same as lineNr
+     */
+    endLineNr: number;
+    /**
+     * 0-based index of the column in the ending line
+     */
+    endColumnNr: number;
+    /**
+     * Severity of the issue
+     */
     severity: "info" | "warning" | "error";
+    /**
+     * Message describing the issue
+     */
     message: string;
 }
 
--- src/workers/python/papyros/linting.py
+++ src/workers/python/papyros/linting.py
@@ -1,18 +1,14 @@
+# Ensure pylint can find the plugin files
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from io import StringIO
 from pylint.lint import Run
 from pylint.reporters.text import TextReporter
 from astroid import MANAGER
-# define categories used for pylint messages
-MESSAGE_CATEGORIES = {
-    'F': 'fatal',
-    'E': 'error',
-    'W': 'warning',
-    'C': 'convention',
-    'R': 'refactor'
-}
+
 PYLINT_RC_FILE = os.path.abspath("papyros/pylint_config.rc")
-PYLINT_PLUGINS = os.path.abspath("papyros/pylint_ast_checker")
+PYLINT_PLUGINS = "pylint_ast_checker"
 
 def lint(file):
     # Ensure file content is correct
@@ -21,7 +17,8 @@ def lint(file):
     Run([
         "-j", "1", # ensure no parallellism is used as we don't have such resources in a worker
         "--rcfile", PYLINT_RC_FILE,
-        '--load-plugins', PYLINT_PLUGINS,
+        "--load-plugins", PYLINT_PLUGINS,
+        "--msg-template", "{path}:{line}:{column}:{end_line}:{end_column}:{category}:{msg}",
         file], reporter=TextReporter(pylint_output), do_exit=False)
     return process_pylint_output(pylint_output.getvalue()) 
 
@@ -29,17 +26,17 @@ def process_pylint_output(linting_output):
     diagnostics = []
     for line in linting_output.split("\n"):
         line: str = line.rstrip()
-        # filename:line:column: error_code: message (message-code)
-        if line.count(":") == 4:
-            _, line_nr, column_nr, code, message = line.rstrip().split(":")
+        # {path}:{line}:{column}:{end_line}:{end_column}:{category}:{msg}
+        if line.count(":") == 6:
+            _, line_nr, column_nr, end_line, end_column, severity, message = line.rstrip().split(":")
             line_nr = int(line_nr)
             column_nr = int(column_nr)
-            code = code.strip()
-            first_bracket = message.index("(")
-            # ignore message code for now
-            message = message[:first_bracket-1]
-            severity = MESSAGE_CATEGORIES[code[0]]
-            diagnostics.append(
-                {"lineNr": line_nr, "columnNr": column_nr, "severity": severity, "message": message}
-            )
+            diagnostics.append({
+                "lineNr": line_nr,
+                "columnNr": column_nr,
+                "endLineNr": int(end_line) if end_line else line_nr,
+                "endColumnNr": int(end_column) if end_column else column_nr,
+                "severity": severity,
+                 "message": message
+            })
     return diagnostics
