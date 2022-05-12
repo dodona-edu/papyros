cb29722b8dcee160b97c1423db97966d2fd92888
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-05-12 11:53:46 +0200

    Add keybind to run code

--- src/CodeRunner.ts
+++ src/CodeRunner.ts
@@ -88,7 +88,7 @@ export class CodeRunner extends Renderable<CodeRunnerRenderOptions> {
     constructor(programmingLanguage: ProgrammingLanguage) {
         super();
         this.programmingLanguage = programmingLanguage;
-        this.editor = new CodeEditor();
+        this.editor = new CodeEditor(() => this.runCode(this.editor.getCode()));
         this.inputManager = new InputManager(async (input: string) => {
             const backend = await this.backend;
             backend.writeMessage(input);
