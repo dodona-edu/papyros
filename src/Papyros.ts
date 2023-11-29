c2fcb71c93f0770ce95fc799eec00900548427c5
jorg-vr <jorg.vr@live.be>
2023-11-29 15:44:32 +0100

    Register serviceworker at root

--- src/Papyros.ts
+++ src/Papyros.ts
@@ -203,7 +203,7 @@ export class Papyros extends Renderable<PapyrosRenderOptions> {
             this.config.channelOptions.scope = serviceWorkerRoot;
             const serviceWorkerUrl = serviceWorkerRoot + serviceWorkerName;
             try {
-                await window.navigator.serviceWorker.register(serviceWorkerUrl);
+                await navigator.serviceWorker.register(serviceWorkerUrl, { scope: "/" });
                 BackendManager.channel = makeChannel({ serviceWorker: this.config.channelOptions })!;
             } catch (error: any) {
                 return false;
