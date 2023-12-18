a9b5f881f5ad943ad2ac8d859e5c1f65e809a5df
jorg-vr <jorg.vr@live.be>
2023-12-18 15:54:18 +0100

    Speedup by buffering frames

--- src/TraceViewer.ts
+++ src/TraceViewer.ts
@@ -4,6 +4,7 @@ import { TraceComponent } from "@dodona/trace-component";
 import { BackendManager } from "./BackendManager";
 import { BackendEventType } from "./BackendEvent";
 import "@dodona/trace-component";
+import { Frame } from "@dodona/trace-component/dist/trace_types";
 
 const TRACE_COMPONENT_ID = "trace-component";
 
@@ -13,11 +14,21 @@ export type FrameState = {
     inputs: number;
 };
 
+function createDelayer(): (callback: () => void, ms: number) => void {
+    let timer: any;
+    return (callback, ms) => {
+        clearTimeout(timer);
+        timer = setTimeout(callback, ms);
+    };
+}
+const delay = createDelayer();
+
 export class TraceViewer extends Renderable<RenderOptions> {
     private frameStates: FrameState[] = [];
     private currentOutputs: number = 0;
     private currentInputs: number = 0;
     private traceComponent: TraceComponent | undefined;
+    private traceBuffer: Frame[] = [];
 
     constructor() {
         super();
@@ -49,7 +60,12 @@ export class TraceViewer extends Renderable<RenderOptions> {
                 inputs: this.currentInputs
             };
             this.frameStates.push(frameState);
-            this.traceComponent?.addFrame(frame);
+            this.traceBuffer.push(frame);
+            if (this.traceBuffer.length > 100) {
+                this.clearBuffer();
+            } else {
+                delay(() => this.clearBuffer(), 100);
+            }
             BackendManager.publish({
                 type: BackendEventType.FrameChange,
                 data: frameState
@@ -79,5 +95,12 @@ export class TraceViewer extends Renderable<RenderOptions> {
         if (this.traceComponent) {
             this.traceComponent.trace = [];
         }
+    }
+
+    public clearBuffer(): void {
+        for (const frame of this.traceBuffer) {
+            this.traceComponent?.addFrame(frame);
+        }
+        this.traceBuffer = [];
     }
 }
