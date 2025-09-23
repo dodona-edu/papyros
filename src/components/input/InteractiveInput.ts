a02b01db6642217f12f0b05444ab192e426e8cd4
jorg.vr <jorg.vanrenterghem@ugent.be>
2025-09-23 13:53:50 +0200

    Focus input when awaiting input

--- src/components/input/InteractiveInput.ts
+++ src/components/input/InteractiveInput.ts
@@ -1,5 +1,6 @@
 import {customElement, property} from "lit/decorators.js";
-import {html, TemplateResult} from "lit";
+import {createRef, Ref, ref} from "lit/directives/ref.js";
+import {html, PropertyValues, TemplateResult} from "lit";
 import {t} from "../../util/Util";
 import {PapyrosElement} from "../helpers/PapyrosElement";
 
@@ -7,10 +8,18 @@ import {PapyrosElement} from "../helpers/PapyrosElement";
 export class InteractiveInput extends PapyrosElement {
     @property({state: true})
     value: string = '';
+    inputRef: Ref<HTMLInputElement> = createRef();
 
     provideInput(): void {
         this.papyros.io.provideInput(this.value);
         this.value = '';
+    }
+
+    protected override updated(_changedProperties: PropertyValues) {
+        super.updated(_changedProperties);
+        if (this.papyros.io.awaitingInput) {
+            this.inputRef.value!.focus();
+        }
     }
 
     protected override render(): TemplateResult {
@@ -26,6 +35,7 @@ export class InteractiveInput extends PapyrosElement {
                    }}
                    placeholder=${this.papyros.io.prompt || t(`Papyros.input_placeholder.interactive`)}
                    ?disabled=${!this.papyros.io.awaitingInput}
+                   ${ref(this.inputRef)}
             >
             <button @click=${() => this.provideInput()} 
                     ?disabled=${!this.papyros.io.awaitingInput}>
