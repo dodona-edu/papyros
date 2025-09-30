fddfaae11fee550278d5c9e3f4cc2a59266746f3
jorg.vr <jorg.vanrenterghem@ugent.be>
2025-09-30 13:08:24 +0200

    Improve wait for serviceworker

--- test/helpers.ts
+++ test/helpers.ts
@@ -10,6 +10,13 @@ export async function waitForOutput(papyros: Papyros, count: number = 1, timeout
     }
 }
 
-export async function waitForInputReady(): Promise<void> {
+export async function waitForInputReady(timeout = 2000): Promise<void> {
     await navigator.serviceWorker.ready;
+    const start = Date.now();
+    while (!navigator.serviceWorker.controller) {
+        if (Date.now() - start > timeout) {
+            throw new Error("Timeout waiting for service worker to control this page");
+        }
+        await new Promise(r => setTimeout(r, 20));
+    }
 }
