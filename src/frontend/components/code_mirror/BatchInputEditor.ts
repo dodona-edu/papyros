13744e63d266773c61c9e26ce6da89b115f78a21
jorg.vr <jorg.vanrenterghem@ugent.be>
2025-10-02 10:15:05 +0200

    Fix linting

--- src/frontend/components/code_mirror/BatchInputEditor.ts
+++ src/frontend/components/code_mirror/BatchInputEditor.ts
@@ -1,13 +1,11 @@
 import { customElement, property } from "lit/decorators.js";
 import { CodeMirrorEditor } from "./CodeMirrorEditor";
 import { EditorView, keymap } from "@codemirror/view";
-import { css, CSSResult } from "lit";
 import { defaultKeymap } from "@codemirror/commands";
 import { setUsedLines, usedLineExtension } from "./Extensions";
 
 @customElement("p-batch-input-editor")
 export class BatchInputEditor extends CodeMirrorEditor {
-
     @property({ type: Number })
     set usedLines(value: number) {
         const lines = Array.from({ length: value }, (a, i) => i+1)
