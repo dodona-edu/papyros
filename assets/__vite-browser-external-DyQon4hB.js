cde8db45ef070f27ce7c453542bae394732ff0a7
chvp <charlotte@vanpetegem.be>
2026-06-02 08:26:15 +0000

    Deploying to gh-pages from @ dodona-edu/papyros@3cf17b2a7d2c3984483301612ad6ddba4b693cb9 🚀

--- assets/__vite-browser-external-DyQon4hB.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-C6AXdu3T.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ assets/__vite-browser-external-ho7OuyLD.js
@@ -0,0 +1,1 @@
+import{t as e}from"./worker-D0QEkFJY.js";var t=e(((e,t)=>{t.exports={}}));export default t();
