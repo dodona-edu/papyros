c942c0bfa25eb5ae9bc1801022eca9c2cddab0fc
bmesuere <mesuerebart@gmail.com>
2026-08-01 13:34:04 +0000

    Deploying to gh-pages from @ dodona-edu/papyros@bf80cd077adae6613a110951545ab78ff36e1d5d 🚀

--- assets/__vite-browser-external-BFwZ7t-G.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-6p5-9lhc.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ assets/__vite-browser-external-i9u1TuOG.js
@@ -0,0 +1,1 @@
+import{t as e}from"./worker-Y61STYpw.js";var t=e(((e,t)=>{t.exports={}}));export default t();
